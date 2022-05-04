339b192fdf05928c33056cd445f2c84403a43beb
Younes-Erraji <younes.erraji8@gmail.com>
2022-05-04 09:36:10 +0000

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+import "./style/bootstrap.min.css";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const [movie, setMovie] = useState("");
+  const [movieDesc, setMovieDesc] = useState("");
+  const [movieRate, setMovieRate] = useState(0);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get")
+      // .then((response) => response.json())
+      .then((response) => console.log(response));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/api/insert", {
+      movie,
+      movieDesc,
+      movieRate,
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="col-12 text-center">CRUD Application</h1>
+        <hr className="col-12" />
+        <form className="col-12" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="movie-name">Movie</label>
+            <input
+              type="text"
+              id="movie-name"
+              className="form-control"
+              value={movie}
+              onChange={(e) => setMovie(e.target.value)}
+            />
+            {/*
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+            */}
+          </div>
+          <div className="form-group">
+            <label htmlFor="movie-desc">Movie Description</label>
+            <input
+              type="text"
+              id="movie-desc"
+              className="form-control"
+              value={movieDesc}
+              onChange={(e) => setMovieDesc(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="movie-rate">Rate</label>
+            <input
+              type="number"
+              id="movie-rate"
+              className="form-control"
+              value={movieRate}
+              onChange={(e) => setMovieRate(e.target.value)}
+            />
+          </div>
+          {/*
+          <div class="form-group form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            />
+            <label class="form-check-label" for="exampleCheck1">
+              Is out
+            </label>
+          </div>
+          */}
+
+          <div className="text-center my-2">
+            <strong>{movie}</strong>
+            <br />
+            <strong>{movieDesc}</strong>
+            <br />
+            <strong>{movieRate}</strong>
+          </div>
+          <button type="submit" className="btn btn-primary btn-lg btn-block">
+            Save
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express"),
+  app = express(),
+  // cors = require('cors),
+  bodyParser = require("body-parser"),
+  mysql = require("mysql"),
+  DB = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "react_movies",
+  });
+
+// app.get("/api/insert", (request, response) => {
+//   const now = Date.now();
+//   const sql = `Insert Into TMovies(Movie_name, Movie_desc, Movie_rate, Movie_views, Inserted_at) values('First Movie', 'First Movie Description', 8, 0, ${now})`;
+//   DB.query(sql, (error, result) => {});
+//   response.send("Hello World!");
+// });
+
+// app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (request, response) => {
+  const movie = request.body.movie,
+    movieDesc = request.body.movieDesc,
+    movieRate = request.body.movieRate,
+    now = Date.now(),
+    sql = `Insert Into TMovies(Movie_name, Movie_desc, Movie_rate, Movie_views, Inserted_at) values('${movie}', '${movieDesc}', ${movieRate}, 0, '${now}')`;
+  DB.query(sql, (error, result) => {
+    console.log(result);
+  });
+});
+
+app.get("/api/get", (request, response) => {
+  const sql = `SELECT Movie_name, Movie_desc, Movie_rate, Movie_views, Inserted_at FROM TMovies`;
+  DB.query(sql, (error, result) => {
+    response.send(result);
+  });
+});
+
+app.listen(3001, () => console.warn("running on port 3001 ..."));
